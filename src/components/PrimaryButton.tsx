d7d86935dd79ac23cf6ecab116607ccda1eae704
Maryan Stoykov <stojkovmarjan@gmail.com>
2025-01-20 22:09:08 +0100

    something

--- src/components/PrimaryButton.tsx
+++ src/components/PrimaryButton.tsx
@@ -1,14 +1,29 @@
-import { View, Text} from "react-native";
+import { View, Text, Pressable } from "react-native";
 
 type PrimaryButtonProps = {
-    children: React.ReactNode
-}
-const PrimaryButton = ({children}:PrimaryButtonProps) => {
-    return (
-        <View>
-            <Text>{children}</Text>
-        </View>
-    );
-}
+  children: React.ReactNode;
+};
+const PrimaryButton = ({ children }: PrimaryButtonProps) => {
+  return (
+    <Pressable>
+      <View style={styles.container}>
+        <Text>{children}</Text>
+      </View>
+    </Pressable>
+  );
+};
+
+const styles = ({
+    container: {
+        backgroundColor: "#db2763",
+        paddingVertical: 12,
+        paddingHorizontal: 30,
+        borderRadius: 25,
+        borderWidth: 1,
+        borderColor: "white",
+        width: 120,
+        alignContent: 'center',
+    }
+});
 
 export default PrimaryButton;
--- src/screens/StartGameScreen.tsx
+++ src/screens/StartGameScreen.tsx
@@ -1,4 +1,10 @@
-import { ImageBackground, StyleSheet, Text, TextInput, View } from "react-native";
+import {
+  ImageBackground,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
 import PrimaryButton from "../components/PrimaryButton";
 import { SafeAreaView, SafeAreaProvider } from "react-native-safe-area-context";
 import React from "react";
@@ -12,9 +18,12 @@ const StartGameScreen = () => {
       <StatusBar style="light" />
       <ImageBackground source={image} resizeMode="cover" style={styles.image}>
         <SafeAreaView style={styles.container}>
+          <Text style={styles.label}>Enter Your number</Text>
           <TextInput style={styles.input} />
-          <PrimaryButton>Reset</PrimaryButton>
-          <PrimaryButton>Confirm</PrimaryButton>
+          <View style={styles.buttonsContainer}>
+            <PrimaryButton>Reset</PrimaryButton>
+            <PrimaryButton>Confirm</PrimaryButton>
+          </View>
         </SafeAreaView>
       </ImageBackground>
     </SafeAreaProvider>
@@ -37,7 +46,17 @@ const styles = StyleSheet.create({
     borderBottomColor: "#CCC",
     marginBottom: 20,
     textAlign: "center",
-    fontSize: 24
+    fontSize: 24,
+    color: "white",
+  },
+  label: {
+    color: "white",
+    fontSize: 24,
+  },
+  buttonsContainer: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    width: "60%",
   },
 });
 
