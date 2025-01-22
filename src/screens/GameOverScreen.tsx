e2d7c81a31c0f0b08f40a9d7cb464eee6a968bf7
stojkovmarjan <stojkovmarjan@gmail.com>
2025-01-22 20:18:12 +0100

    the end

--- src/screens/GameOverScreen.tsx
+++ src/screens/GameOverScreen.tsx
@@ -1,30 +1,86 @@
-import { RouteProp, useRoute } from "@react-navigation/native";
+import {
+  RouteProp,
+  useRoute,
+  useNavigation,
+  NavigationProp,
+  StackActions,
+} from "@react-navigation/native";
 import { StatusBar } from "expo-status-bar";
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet, Image } from "react-native";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 import BackgroundWrapper from "../BackgroundWrapper";
+import PrimaryButton from "../components/PrimaryButton/PrimaryButton";
+
 type GameOverScreenRouteProps = {
   rounds: number;
   theNumber: number;
 };
 const GameOverScreen = () => {
+  const navigation = useNavigation<NavigationProp<any>>();
   const route = useRoute<RouteProp<{ params: GameOverScreenRouteProps }>>();
   const { rounds, theNumber } = route.params;
 
-
   return (
     <SafeAreaProvider>
-       <StatusBar style="light" />
-       <BackgroundWrapper>
-      <SafeAreaView>
-        <Text>The Game is Over!</Text>
-        <Text>
-          Your phone guessed the number {theNumber} in {rounds} rounds.
-        </Text>
-      </SafeAreaView>
+      <StatusBar style="light" />
+      <BackgroundWrapper>
+        <SafeAreaView style={styles.safeArea}>
+          <View style={styles.gameOverLabelContainer}>
+            <Text style={styles.label}>Game Over!</Text>
+          </View>
+          <Image
+            source={require("../../assets/images/success.png")}
+            style={styles.image}
+          />
+          <Text style={styles.scoreText}>
+            Your phone guessed the number {theNumber} in {rounds} rounds.
+          </Text>
+          <PrimaryButton
+            buttonHeight={40}
+            buttonWidth={120}
+            onPress={() => {
+              navigation.dispatch(StackActions.pop(1));
+            }}
+          >
+            Start New Game
+          </PrimaryButton>
+        </SafeAreaView>
       </BackgroundWrapper>
     </SafeAreaProvider>
   );
 };
+
+const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1,
+    alignItems: "center",
+  },
+  gameOverLabelContainer: {
+    width: "80%",
+    height: 60,
+    justifyContent: "center",
+    alignContent: "center",
+    alignItems: "center",
+    marginVertical: 40,
+    borderWidth: 1,
+    borderColor: "white",
+  },
+  image: {
+    width: 250,
+    height: 250,
+    borderRadius: 125,
+  },
+
+  label: {
+    color: "white",
+    fontSize: 24,
+  },
+  scoreText: {
+    color: "white",
+    fontSize: 20,
+    textAlign: "center",
+    margin: 20,
+  },
+});
 
 export default GameOverScreen;
